1232d77042aa17b09f49722c17a32fcff7eef637
sofia <sonrud@mail.ru>
2023-01-03 23:18:05 +0300

    add 56

---
+++ task56/Program.cs
@@ -0,0 +1,55 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); 
+    }
+}
+
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int Release(int[,] matrix)
+{
+    int min = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+        min += matrix[0, i];
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        if (min > sum)
+            min = sum;
+    }
+    return min;
+}
+
+
+Console.Clear();
+Console.Write("Введите размер прямоугольного двумерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] == size[1])
+{
+    Console.Write("Вы ошиблись! ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный двумерный массив: ");
+Print(matrix);
+Console.WriteLine();
+Console.WriteLine($"Результат: {Release(matrix)}");
